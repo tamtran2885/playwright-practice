e4205b357f3231f961d903a44498d0be125407a5
Tam Tran <tamtran2885@gmail.com>
2022-11-23 19:02:52 +0100

    turn on headless mode

--- tests/record_example.spec.js
+++ tests/record_example.spec.js
@@ -3,7 +3,7 @@ const { chromium } = require("@playwright/test");
 
 test("Test recorded automatically by Playwright inspector", async () => {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
   const context = await browser.newContext();
   const page = await context.newPage();
